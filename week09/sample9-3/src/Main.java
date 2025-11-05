62fccbee8743c5efe7a61a6fd9e0fc3633dd453d
USER <a01056726541@gmail.com>
2025-11-05 15:32:05 +0900

    9주차

---
+++ week09/sample9-3/src/Main.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        final int COUNT = 5;
+        int grade;
+        int total = 0;
+        float avg;
+
+        int i = 1;
+        while (i <= COUNT) {
+            System.out.printf("%,d번 학생 성적 입력 : ", i);
+            grade = keyboard.nextInt();
+            if (grade >= 0 && grade <= 100) {
+                total += grade;
+                i++;
+            } else {
+                System.err.println("ERROR : 0 ~ 100");
+                System.in.read();
+            }
+        }
+
+        avg = total / (float)COUNT;
+
+        System.out.printf("총점 : %,d 점\n", total);
+        System.out.printf("평균 : %.2f 점\n", avg);
+    }
+}
